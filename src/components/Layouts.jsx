20f3ed0f07bc6e8d69a41e28f0da7c21e26cf00a
John Mason <johnmsn22@gmail.com>
2021-10-15 04:58:25 +0100

    responsive

--- src/components/Layouts.jsx
+++ src/components/Layouts.jsx
@@ -2,10 +2,18 @@ import styled from 'styled-components';
 
 export const MainLayout = styled.div`
     padding: 0.8rem 5rem;
+    @media screen and (max-width: 768px){
+        padding: 0.8rem 0.1rem;
+        padding-top: 62px;
+        padding-bottom:0;
+    }
 `
 
 export const InnerLayout = styled.div`
     padding: 1rem 5rem;
+    @media screen and (max-width: 768px){
+        padding: 0.8rem 0.3rem;
+    }
 `
 
 export const SectionLayout = styled.div`
@@ -13,4 +21,8 @@ export const SectionLayout = styled.div`
     background: #fff;
     border-radius: 3px;
     margin-bottom: 1rem;
+    
+    @media screen and (max-width: 768px){
+        padding: 2rem 0.8rem;
+    }
 `
